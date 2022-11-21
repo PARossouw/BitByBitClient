d86de5ca6fd4fca70785c369d90ea8f0710fe968
tarunsing <tsing53@gmail.com>
2022-11-21 14:00:16 +0200

    story

--- BitByBitClient/src/main/java/Servlets/StoryServlet.java
+++ BitByBitClient/src/main/java/Servlets/StoryServlet.java
@@ -14,6 +14,7 @@ import jakarta.annotation.ManagedBean;
 import jakarta.servlet.RequestDispatcher;
 
 import jakarta.servlet.http.HttpSession;
+import java.util.List;
 
 @WebServlet(name = "StoryServlet", urlPatterns = {"/StoryServlet"})
 @ManagedBean
