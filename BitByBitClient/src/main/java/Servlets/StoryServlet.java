ec665eb4fc6e050191d56a2ba34b185a8f4344e8
AntonMetrowich <ametrowich@gmail.com>
2022-11-25 20:08:17 +0200

    A

--- BitByBitClient/src/main/java/Servlets/StoryServlet.java
+++ BitByBitClient/src/main/java/Servlets/StoryServlet.java
@@ -14,6 +14,7 @@ import User_Interactions.Comment.Model.Comment;
 import jakarta.annotation.ManagedBean;
 import jakarta.servlet.RequestDispatcher;
 import RestClientRemoteController.RestClientCategory;
+import RestClientRemoteController.RestClientSMS;
 import RestClientRemoteController.RestClientStory_Transaction;
 import SMS.smsreq;
 import User.Model.User;
@@ -22,8 +23,8 @@ import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import java.io.File;
+import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,11 +44,13 @@ public class StoryServlet extends HttpServlet {
     private User user;
     private String message;
     private RequestDispatcher rd;
+    private RestClientSMS restClientSms;
 
     public StoryServlet() {
         this.restClientStory = new RestClientStory("http://localhost:8080/RIP/RIP");
         this.restClientCategory = new RestClientCategory("http://localhost:8080/RIP/RIP");
         this.restClientStory_Transaction = new RestClientStory_Transaction("http://localhost:8080/RIP/RIP");
+        this.restClientSms = new RestClientSMS("http://196.41.180.157:8080/sms/sms_request");
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -124,26 +127,20 @@ public class StoryServlet extends HttpServlet {
                 break;
             case ("Search"):
                 break;
-                
-           
-                 case ("Create Story"):
+
+            case ("Create Story"):
                 Story storyCreate = new Story(1, "Create Story", "mock create writer", "mock description", "mock imagepath", "mock body", false, true, cal, true, true, 10, 57, 4.0);
 
-
-                
                 request.setAttribute("story", storyCreate);
                 Comment commentCreate = new Comment();
                 commentCreate.setCommentBody("Mock comment body");
                 request.setAttribute("comment", commentCreate);
-
 
                 session.getAttribute("user");
                 RequestDispatcher rdCreate = request.getRequestDispatcher("createStory.jsp");
                 rdCreate.forward(request, response);
 
                 break;
-                
-                
 
             case ("Like Story"):
                 Story storyView1 = new Story();
@@ -188,7 +185,7 @@ public class StoryServlet extends HttpServlet {
 
                 //List<Story> storyList = restClientStory.getFiveStoriesForStoryOfTheDay();
                 break;
-            case ("Approve"):
+            case "Approve":
 
                 user = (User) session.getAttribute("user");
                 Story storyToApprove = this.storyToReview;
@@ -205,47 +202,51 @@ public class StoryServlet extends HttpServlet {
                     if (i == storyReviewList.size() - 1) {
                         request.setAttribute("storyReview", null);
                         rd = request.getRequestDispatcher("Editor.jsp");
-                        rd.forward(request, response);
+                        //rd.forward(request, response);
                         break;
-                    }
-
-                    else if (this.storyToReview == storyReviewList.get(i)) {
+                    } else if (this.storyToReview == storyReviewList.get(i)) {
 
                         this.storyToReview = storyReviewList.get(i + 1);
                         request.setAttribute("storyReview", this.storyToReview);
                         rd = request.getRequestDispatcher("Editor.jsp");
-                        rd.forward(request, response);
+                        //rd.forward(request, response);
                         break;
                     }
-                    
+
                 }
-                    //send sms CHANGING THE DATE TO TEST
-                    smsreq sms = new smsreq();
-                    //sms.setDatetime(new Date(2022,11,24,23,0,0));
-                    sms.setDatetime("2022/05/20,10:00:00");
-                    sms.setUser(1);
-                    sms.setPass("password");
-                    sms.setMsisdn("0739068691");
-                    sms.setMessage("test message");
-                    
-            try {
-                JAXBContext jaxBContext = JAXBContext.newInstance(smsreq.class);
+                //send sms CHANGING THE DATE TO TEST
+                smsreq sms = new smsreq();
+                //sms.setDatetime(new Date(2022,11,24,23,0,0));
+                sms.setDatetime("2022/05/20,10:00:00");
+                sms.setUser("GROUP2");
+                sms.setPass("2group");
+                sms.setMsisdn("0716772150");
+                sms.setMessage("test message");
+
                 
-                Marshaller marshaller = jaxBContext.createMarshaller();
-                
-                File xmlOutput = new File("C:\\Users\\ametr\\Desktop\\xmlTestOutput.xml");
-                marshaller.marshal(sms, xmlOutput);
-                
-                //dunno about this
-                rd = request.getRequestDispatcher("http://196.41.180.157:8080/sms/sms_request");
-                request.setAttribute("smsreq", xmlOutput);
-                rd.forward(request, response);
-                
-                
-                
-            } catch (JAXBException ex) {
-                Logger.getLogger(StoryServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                try {
+                    JAXBContext jaxBContext = JAXBContext.newInstance(smsreq.class);
+
+                    Marshaller marshaller = jaxBContext.createMarshaller();
+
+                    File xmlOutput = new File("C:\\Users\\ametr\\Desktop\\xmlTestOutput.xml");
+                    marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+                    StringWriter sw = new StringWriter();
+                    marshaller.marshal(sms, sw);
+                    String smsResponse = restClientSms.sendMessage(sw);//have another argument here that has the actual message
+                    //String smsResponse = "testmctesty";
+                    request.setAttribute("smsResponse", smsResponse);
+                    rd = request.getRequestDispatcher("Editor.jsp");
+                    rd.forward(request, response);
+
+
+//dunno about this
+//                rd = request.getRequestDispatcher("http://196.41.180.157:8080/sms/sms_request");
+//                request.setAttribute("smsreq", xmlOutput);
+//                rd.forward(request, response);
+                } catch (JAXBException ex) {
+                    Logger.getLogger(StoryServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
 
                 break;
             case ("Reject"):
