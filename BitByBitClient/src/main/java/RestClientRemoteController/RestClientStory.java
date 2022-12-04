21bfdcd444395e7611ce0bd4dd64dafe80f48a64
AntonMetrowich <ametrowich@gmail.com>
2022-12-04 11:34:09 +0200

    GGs

--- BitByBitClient/src/main/java/RestClientRemoteController/RestClientStory.java
+++ BitByBitClient/src/main/java/RestClientRemoteController/RestClientStory.java
@@ -3,6 +3,7 @@ package RestClientRemoteController;
 import Story.Model.Story;
 import User.Model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
